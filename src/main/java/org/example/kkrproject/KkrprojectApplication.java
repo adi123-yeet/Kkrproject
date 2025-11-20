9d7980bda86c583826e6de2835dffa89480f7371
adi <aditya.kandhai@gmail.com>
2025-11-20 13:51:52 +0100

    test v1

---
+++ src/main/java/org/example/kkrproject/KkrprojectApplication.java
@@ -0,0 +1,13 @@
+package org.example.kkrproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkrprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkrprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kkrproject/KkrprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kkrproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkrprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
